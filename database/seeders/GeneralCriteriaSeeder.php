3ee2aa3a6f3db91db1026bf3502f7bf61c0bfa8e
Ahmed Yaghi <yaghiahmed91@gmail.com>
2026-01-20 11:30:39 +0300

    updates

--- database/seeders/GeneralCriteriaSeeder.php
+++ database/seeders/GeneralCriteriaSeeder.php
@@ -12,17 +12,16 @@ class GeneralCriteriaSeeder extends Seeder
      */
     public function run(): void
     {
-        GeneralCriteria::create(['title' => 'الالتزام بالحضور والتصرف', 'type' => 1]);
+        GeneralCriteria::create(['title' => 'الالتزام بالحضور والانصراف', 'type' => 1]);
         GeneralCriteria::create(['title' => 'اللباقة والتواصل', 'type' => 1]);
-        GeneralCriteria::create(['title' => 'التعاون مع الفريق', 'type' => 1]);
         GeneralCriteria::create(['title' => 'المبادرة والمشاركة', 'type' => 1]);
-        GeneralCriteria::create(['title' => 'التعامل مع المستفيدين', 'type' => 1]);
+        GeneralCriteria::create(['title' => 'حسن التعامل مع المستفيدين', 'type' => 1]);
         GeneralCriteria::create(['title' => 'احترام السياسات الداخلية', 'type' => 1]);
         GeneralCriteria::create(['title' => 'الالتزام بالمظهر والسلوك العام', 'type' => 1]);
-        GeneralCriteria::create(['title' => 'الانضباط', 'type' => 2]);
-        GeneralCriteria::create(['title' => 'المبادرة', 'type' => 2]);
-        GeneralCriteria::create(['title' => 'جودة التنفيذ', 'type' => 2]);
-        GeneralCriteria::create(['title' => 'التواصل والتعاون', 'type' => 2]);
+        GeneralCriteria::create(['title' => 'الانضباط في أداء وتسليم المهمة', 'type' => 2]);
+        GeneralCriteria::create(['title' => 'الابتكار في حل المشكلات', 'type' => 2]);
+        GeneralCriteria::create(['title' => 'جودة المخرجات', 'type' => 2]);
+        GeneralCriteria::create(['title' => 'العمل الجماعي', 'type' => 2]);
         GeneralCriteria::create(['title' => 'تحمل المسؤولية', 'type' => 2]);
         GeneralCriteria::create(['title' => 'مهارات العرض', 'type' => 3]);
         GeneralCriteria::create(['title' => 'المبادرة الذاتية', 'type' => 3]);
