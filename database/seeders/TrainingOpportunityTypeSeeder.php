f563e3e35fb9c9914f65622ebf41028ec2cab3ec
Ahmed Yaghi <yaghiahmed91@gmail.com>
2026-03-30 13:50:43 +0300

    updates

--- database/seeders/TrainingOpportunityTypeSeeder.php
+++ database/seeders/TrainingOpportunityTypeSeeder.php
@@ -7,14 +7,42 @@ use Illuminate\Database\Seeder;
 
 class TrainingOpportunityTypeSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        TrainingOpportunityType::create(['title' => 'تقنية']);
-        TrainingOpportunityType::create(['title' => 'إدارية']);
-        TrainingOpportunityType::create(['title' => 'مهنية']);
+        TrainingOpportunityType::truncate();
+        $types = [
+            'إداري',
+            'تقني',
+            'ميداني',
+            'مالي ومحاسبي',
+            'قانوني',
+            'إعلامي وعلاقات عامة',
+            'تسويقي',
+            'موارد بشرية',
+            'بحوث ودراسات',
+            'تعليمي وتدريبي',
+            'صحي',
+            'اجتماعي',
+            'هندسي',
+            'تشغيلي ولوجستي',
+            'تقنية معلومات',
+            'خدمة مستفيدين',
+            'قيادي وإشرافي',
+            'مكتبي',
+            'إدارة مشاريع',
+            'جمع تبرعات / تنمية موارد',
+            'إدارة المتطوعين',
+            'تحليل بيانات',
+            'قياس أثر',
+            'ابتكار خدمات',
+            'مسؤول استثمار اجتماعي',
+            'أخرى',
+        ];
 
+        foreach ($types as $type) {
+            TrainingOpportunityType::create([
+                'title' => $type,
+            ]);
+        }
     }
 }
