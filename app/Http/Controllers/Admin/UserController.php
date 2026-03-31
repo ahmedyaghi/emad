e8f916fb3411a6d09e3dc58d188373accbaf2eb0
Ahmed Yaghi <yaghiahmed91@gmail.com>
2026-03-31 13:16:40 +0300

    updates

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Enums\UserStatus;
+use App\Exports\UsersExport;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\CreateUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Spatie\Permission\Models\Role;
 use Maatwebsite\Excel\Facades\Excel;
-use App\Exports\UsersExport;
+use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
@@ -49,7 +49,6 @@ class UserController extends Controller
 
     public function update(Request $request, User $user)
     {
-        // لو في تغيير حالة فقط
         if ($request->has('status')) {
 
             $user->status = UserStatus::from($request->status);
@@ -58,7 +57,6 @@ class UserController extends Controller
             return back()->with('success', 'تم تحديث حالة المستخدم');
         }
 
-        // تحديث بيانات المستخدم العادية
         $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users,email,' . $user->id,
@@ -70,10 +68,8 @@ class UserController extends Controller
             'phone' => $request->phone,
         ]);
 
-        return redirect()->route('admin.users.index')
-            ->with('success', 'تم تحديث بيانات المستخدم');
+        return redirect()->route('admin.users.index')->with('success', 'تم تحديث بيانات المستخدم');
     }
-
 
     public function update_status($status, $id)
     {
